46855fde2954e040cbde3a497a6801821e067de7
Lucio <104533177+LucioBelloni@users.noreply.github.com>
2024-11-24 22:41:12 -0300

    punto_c.sh

--- punto_c.sh
+++ punto_c.sh
@@ -1,22 +1,13 @@
-PARA LA IMAGEN
-docker build -t luciobg110/2parcial-ayso:v1.0 .
-docker image ls
-docker push luciobg110/2parcial-ayso:v1.0
+mkdir docker2Parcial
+cd docker2Parcial/
+mkdir appHomeBanking
+cd appHomeBanking/
+vim index.html
+vim contancto.html
 
-PARA EL HOME BANKING
-vim dockerfile:
-FROM nginx
-COPY appHomeBanking /usr/share/nginx/html
-vim index.html:
-<h1> Este es el titulo de el index del  parcial </h1>
-vim contacto.html:
-<h1> Numero de contacto </h1>
-<h2> Nombre: Fernando </h2>
-<h2> Apellido: Paez </h2>
-<h2> Contacto: 1130067173 </h2>
-docker build -t luciobg110/homebanking:v1.0 .
-docker push luciobg110/homebanking:v1.0
-docker run -d -p 8080:80 luciobg110/2parcial-ayso:v1.0
-docker image ls
-docker container ls
-docker stop 9dba5a2243b0
+vim Dockerfile
+
+sudo docker login -u luciobg110
+sudo docker build -t luciobg110/2parcial-ayso:v1.0 .
+sudo docker push luciobg110/2parcial-ayso:v1.0
+sudo docker run -d -p 8080:80 luciobg110/2parcial-ayso:v1.0
